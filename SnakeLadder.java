066312c66ad353129c1d93be4254eec4b759b813
sarveshped30 <sarveshpednekar30@gmail.com>
2022-04-11 01:36:04 +0530

    Added: snake and ladder UC5

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -7,22 +7,35 @@ public class SnakeLadder {
         int player1 = 0; // player1 position starts with 0
 
         Random rand = new Random();
-        int dieRoll = rand.nextInt(7);      // initializing die with random function
-        int option = rand.nextInt(3);
 
-        System.out.println("Player rolling the die.....");
-        System.out.println("Got " + dieRoll);
+        while(player1 != 100) {
+            int roll = rand.nextInt(6);      // initializing die with random function
+            int dieRoll = roll + 1;
+            int option = rand.nextInt(3);       // initializing option with random function
 
-        switch(option) {
-            case 0:
-                System.out.println("No play.....");
-                break;
-            case 1:
-                System.out.println("Got Ladder.....");
-                player1 += dieRoll;
-            default:
-                System.out.println("oops! Got eaten by snake.....");
-                player1 -= dieRoll;
+            System.out.println("Player rolling the die.....");
+            System.out.println("Got " + dieRoll);
+
+            switch(option) {
+                case 0:
+                    System.out.println("No play.....");
+                    break;
+                case 1:
+                    System.out.println("Got Ladder.....");
+                    player1 += dieRoll;
+                    if(player1 > 100) {
+                        player1 -= dieRoll;
+                    }
+                    break;
+                default:
+                    System.out.println("oops! Got eaten by snake.....");
+                    player1 -= dieRoll;
+            }
+
+            if(player1 < 0) {
+                player1 = 0;
+            }
         }
+        System.out.println("Congratulations you reached " + player1 + " you Won the game.........");
     }
 }
