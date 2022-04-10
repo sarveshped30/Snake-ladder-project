c7ab8e23480e2f32d908603725767b3e2499f52d
sarveshped30 <sarveshpednekar30@gmail.com>
2022-04-11 00:56:37 +0530

    Added: snake ladder UC3

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -8,8 +8,21 @@ public class SnakeLadder {
 
         Random rand = new Random();
         int dieRoll = rand.nextInt(7);      // initializing die with random function
+        int option = rand.nextInt(3);
 
         System.out.println("Player rolling the die.....");
         System.out.println("Got " + dieRoll);
+
+        switch(option) {
+            case 0:
+                System.out.println("No play.....");
+                break;
+            case 1:
+                System.out.println("Got Ladder.....");
+                player1 += dieRoll;
+            default:
+                System.out.println("oops! Got eaten by snake.....");
+                player1 -= dieRoll;
+        }
     }
 }
